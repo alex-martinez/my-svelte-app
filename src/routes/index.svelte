16198ec7e51a2644d35d0c25b4198ca84e6ebef1
Alex Martinez <alexmtz298@gmail.com>
2022-07-31 22:11:40 -0500

    Update to use save button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,7 @@
     html,
     javascript
   };
+  let docTextCache = { ...docText };
 
   $: srcDoc = `
     <html>
@@ -50,59 +51,53 @@
     activeTab = lang;
   }
 
-  const debounce = (func: any, delay: number) => {
-    let timerId: NodeJS.Timeout;
-    return () => {
-      clearTimeout(timerId);
-      timerId = setTimeout(func, delay);
-    };
+  function handleChange(val: string, lang: EditorLanguageType) {
+    docTextCache[lang] = val;
   };
 
-  const handleChange = (val: string, lang: EditorLanguageType) => {
-    docText = {
-      ...docText,
-      [lang]: val
-    };
-    console.log('DOCTEXT:', docText);
-  };
+  function onSave() {
+    docText = docTextCache;
+  }
 </script>
 
 <div class="w-full h-screen flex">
   <div class="w-1/2 bg-slate-800">
-    <nav class="bg-slate-900">
+    <nav class="flex items-center bg-slate-900">
       <button
-        class="py-1 px-3 border-b-2"
+        class="py-2 px-3 border-b-2 hover:border-b-indigo-400"
         class:bg-slate-800={activeTab === 'html'}
         class:border-b-transparent={activeTab !== 'html'}
         class:border-b-indigo-400={activeTab === 'html'}
         on:click={() => changeActiveTab('html')}>HTML</button
       >
       <button
-        class="py-1 px-3 border-b-2"
+        class="py-2 px-3 border-b-2 hover:border-b-indigo-400"
         class:bg-slate-800={activeTab === 'css'}
         class:border-b-transparent={activeTab !== 'css'}
         class:border-b-indigo-400={activeTab === 'css'}
         on:click={() => changeActiveTab('css')}>CSS</button
       >
       <button
-        class="py-1 px-3 border-b-2"
+        class="py-2 px-3 border-b-2 hover:border-b-indigo-400"
         class:bg-slate-800={activeTab === 'javascript'}
         class:border-b-transparent={activeTab !== 'javascript'}
         class:border-b-indigo-400={activeTab === 'javascript'}
         on:click={() => changeActiveTab('javascript')}>JavaScript</button
       >
+
+      <button class="ml-auto rounded-full bg-cyan-600 hover:bg-cyan-700 text-white font-bold text-sm px-2 py-1 mr-2" on:click={onSave}>Save & run</button>
     </nav>
 
     {#if activeTab === 'html'}
-      <Editor doc={docText.html} lang={'html'} onChange={(val) => debounce(handleChange(val, 'html'), 250)} />
+      <Editor doc={docText.html} lang={'html'} onChange={handleChange} />
     {/if}
 
     {#if activeTab === 'javascript'}
-      <Editor doc={docText.javascript} lang={'javascript'} onChange={(val) => debounce(handleChange(val, 'javascript'), 250)} />
+      <Editor doc={docText.javascript} lang={'javascript'} onChange={handleChange} />
     {/if}
 
     {#if activeTab === 'css'}
-      <Editor doc={docText.css} lang={'css'} onChange={(val) => debounce(handleChange(val, 'css'), 250)} />
+      <Editor doc={docText.css} lang={'css'} onChange={handleChange} />
     {/if}
   </div>
 
